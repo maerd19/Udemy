bb1cd3969c650b564960cf1b1234fc88f0a11644
maerd19 <revolutions_333@hotmail.com>
2019-11-20 02:19:29 -0600

    cargando las imagenes condicionalmente

--- react/react-jp/noticiasreact/src/App.js
+++ react/react-jp/noticiasreact/src/App.js
@@ -1,9 +1,44 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import Header from './components/Header';
+import ListaNoticias from './components/ListaNoticias';
 
 class App extends Component {
-  state = {  }
+  state = { 
+    noticias: [] 
+  }
+
+  // Usualmente es recomendado que el llamado a una API se haga en el metodo del ciclo de vida componentDidMount
+  // Que es cuando el documento esta listo
+  componentDidMount() {
+    this.consultarNoticias();
+  }
+
+  consultarNoticias = async () => {
+    const url = `https://newsapi.org/v2/top-headlines?country=mx&category=business&apiKey=7903c60ef84e4129ae15d071e3560fb0`;
+
+    const respuesta = await fetch(url);
+    const noticias = await respuesta.json();
+
+    // Las noticias recibidas a traves de la API se utilizan para llenar el state
+    this.setState({
+      noticias : noticias.articles
+    })
+  }
+
   render() {
-    return (<h1>Noticias API React</h1>)
+    return (
+      <Fragment>
+        <Header 
+          titulo='Noticias REACT API'
+        />
+
+        <div className='container white contenedor-noticias'>
+          <ListaNoticias 
+            noticias={this.state.noticias}
+          />
+        </div>
+      </Fragment>
+    );
   }
 }
 
---
+++ react/react-jp/noticiasreact/src/components/Noticia.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const Noticia = ({noticia}) => {
+
+    // extraer los datos
+    const { source, description, urlToImage, url, title } = noticia;
+
+    // condicionalmente cargar la imagen si esta disponible
+    const imagen = (urlToImage) ? 
+    <div className='card-image'>
+        <img src={urlToImage} alt={title} />
+        <span className='card-title'>{source.name}</span>
+    </div>
+    : null
+
+    return (
+        <div className='col s12 m6 l4'>
+            <div className='card'>                
+                {imagen}
+
+                <div className='card-content'>
+                    <h3>{title}</h3>
+                    <p>{description}</p>
+                </div>
+
+                <div className='card-action'>
+                    <a href={url} target='_blank' rel="noopener noreferrer" className='waves-effect waves-light btn'>
+                        Ver noticia completa
+                    </a>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Noticia
